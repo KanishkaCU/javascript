7578f3f1229323cbbcbe587b0d943e6622a660d3
KanishkaCU <kanishkacu888@gmail.com>
2026-01-28 11:03:00 +0530

    js

---
+++ Start.js
@@ -0,0 +1,83 @@
+
+// 1 
+var price = 100;
+var product = "pen";
+var tax = 20;
+console.log(product);
+var total = price + tax;
+console.log(total);
+
+//2
+var fruit = "Apple";
+var count = "10";
+var price = "50";
+var total = count * price;
+console.log(fruit);
+console.log(total);
+
+//3
+var a = 10;
+var b = ++a;
+
+console.log(a);
+console.log(b);
+var c = a++;
+console.log(c);
+
+//4
+function abc()
+{
+    console.log("abcd")
+}
+ abc()
+
+ //5
+
+var a = 10;
+var b = 15;
+function add()
+{
+    console.log(a+b);
+}
+add()
+
+//6
+
+var factor = "kamal";
+var fplayer = "dhoni";
+var fmovie = "anbe sivam";
+
+function favourite(){
+    console.log("favourite actor:", factor);
+    console.log("favourite player:", fplayer);
+    console.log("favourite movie:", fmovie);
+}
+favourite()
+
+
+//7
+function area(l,b)
+{
+    console.log(l*b);
+}
+area(10,3);
+
+8
+ function add(a,b)
+ {
+    return a+b
+ }
+ var c = add(10,20)
+ console.log(c)
+
+ 9
+
+    var rain = false
+    if(rain)
+    {
+        console.log("unbrella take")
+    }
+    else {
+        console.log("no need")
+    }
+
